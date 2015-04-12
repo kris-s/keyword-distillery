d6a4de207e7ce5d3f88e51bea20d6eb7bf05e773
kris-s <krisshamloo@gmail.com>
2015-04-12 13:07:01 -0400

    add existing keyword data

--- load_toolkit.py
+++ load_toolkit.py
@@ -2,8 +2,6 @@ import csv
 import json
 import requests
 
-keyword_relations = []
-
 
 def load_treasure_csv(filepath='data/treasure.csv'):
     data_object = []
@@ -13,6 +11,7 @@ def load_treasure_csv(filepath='data/treasure.csv'):
             data_object.append(row)
 
     return data_object
+
 
 def generate_treasure_hunt_json():
     json_skeleton = {}
@@ -29,27 +28,6 @@ def generate_treasure_hunt_json():
                                   indent=4,
                                   separators=(',', ': ')))
 
-def generate_existing_keyword_relations_set(data_object):
-    keyword_relations_set = set()
-    for i in range(1, len(data_object)):
-        relation = {'title': data_object[i][5],
-                    'keyword_relations': []}
-        keywords_i = data_object[i][6]
-        for j in range(1, len(data_object)):
-            if i != j and data_object[i][5] != data_object[j][5]:
-                keywords_j = data_object[j][6]
-                if ';' in keywords_i and ';' in keywords_j:
-                    keywords_i = keywords_i.split(';')
-                    keywords_j = keywords_j.split(';')
-                    for word in keywords_i:
-                        if word in keywords_j:
-                            r = {word: data_object[j][5]}
-                            relation['keyword_relations'].append(r)
-                            keyword_set.add(word)
-
-        keyword_relations.append(relation)
-
-    return keyword_relations_set
 
 def generate_existing_keyword_set(data_object):
     keyword_set = set()
@@ -61,6 +39,7 @@ def generate_existing_keyword_set(data_object):
 
     return keyword_set
 
+
 for data_set in keyword_relations:
     if len(data_set['keyword_relations']) > 0:
         for relationship in data_set['keyword_relations']:
@@ -68,6 +47,32 @@ for data_set in keyword_relations:
                                                               relationship.values()[0],
                                                               relationship.keys()[0])
 
+
+def filter_keyword_set(keyword_set):
+    keyword_array = []
+
+    for word in keyword_set.copy():
+        if len(word) < 4:
+            keyword_set.remove(word)
+
+    for word in keyword_set:
+        keyword_array.append(word.lower())
+
+    return keyword_array
+
+
+def write_existing_keywords_json(keyword_array):
+    with open('data/existing_keywords.json', 'w') as jsonfile:
+        jsonfile.write(json.dumps(keyword_array,
+                                  sort_keys=True,
+                                  indent=4,
+                                  separators=(',', ': ')))
+
+
 data_object = load_treasure_csv()
-x = generate_existing_keyword_set(data_object)
-print 'done!'
+keyword_set = generate_existing_keyword_set(data_object)
+keyword_array = filter_keyword_set(keyword_set)
+keyword_array.sort()
+write_existing_keywords_json(keyword_array)
+
+print '*---*\nCreated existing_keywords.json with {} keywords.\n*---*'.format(len(keyword_array))
